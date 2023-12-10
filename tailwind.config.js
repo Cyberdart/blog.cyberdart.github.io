3bf64c247eb5f8a294299612d21bb373fba0a3ec
timetravelthree <dantexor@protonmail.com>
2023-12-10 03:29:16 -0900

    feat: bootstrap initial theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './**/*.html'
-  ],
-  darkMode: 'media',
+  content: ["./**/*.html"],
+  darkMode: "media",
   theme: {
     extend: {
-      typography: {
+      typography: ({ theme }) => ({
         DEFAULT: {
           css: {
+            "--tw-prose-headings": theme("colors.zinc[200]"),
+            "--tw-prose-quotes": theme("colors.zinc[400]"),
+            "--tw-prose-body": theme("colors.zinc[300]"),
+            "--tw-prose-links": theme("colors.teal[400]"),
+            "--tw-prose-th-borders": theme("colors.zinc[700]"),
+            "--tw-prose-td-borders": theme("colors.zinc[700]"),
+
             pre: {
               padding: "0",
               color: "#1F2933",
-              backgroundColor: "#F3F3F3"
+              backgroundColor: "#F3F3F3",
             },
             code: {
               padding: "0.2em 0.4em",
-              backgroundColor: "#F3F3F3",
+              backgroundColor: theme("colors.zinc[800]"),
               color: "#DD1144",
               fontWeight: "400",
-              "border-radius": "0.25rem"
+              "border-radius": "0.25rem",
             },
             "code::before": false,
             "code::after": false,
@@ -28,9 +33,9 @@ module.exports = {
             "blockquote p:last-of-type::after": false,
           },
         },
-      },
+      }),
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
@@ -38,8 +43,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-
-}
+  plugins: [require("@tailwindcss/typography")],
+};
